ccaa4df096be2df37c98c99ff9601d43e929a5bc
keeperofpictures <abuzersfzxc@gmail.com>
2025-02-11 19:20:38 +0300

    stage 5th

--- src/main.cs
+++ src/main.cs
@@ -1,22 +1,31 @@
 using System.Net;
 using System.Net.Sockets;
 
+List<string> builtins = ["exit", "echo", "type"];
+
 while (true)
 {
     Console.Write("$ ");
     var command = Console.ReadLine().Trim();
-    if (command.StartsWith("exit"))
+    var cmdArgs = command.Split(" ");
+    switch (cmdArgs[0])
     {
-        var cmdArgs = command.Split();
-        return int.Parse(cmdArgs[1]);
-    }
-    else if (command.StartsWith("echo"))
-    {
-        if(command.Length > 5)
-            Console.WriteLine(command.Substring(5));
-    }
-    else
-    {
-        Console.WriteLine($"{command}: command not found");
+        case "exit":
+            if (cmdArgs.Length > 1)
+                return int.Parse(cmdArgs[1]);
+            break;
+        case "echo":
+            if (command.Length > 5)
+                Console.WriteLine(command.Substring(5));
+            break;
+        case "type":
+            if (builtins.Contains(cmdArgs[1]))
+                Console.WriteLine($"{cmdArgs[1]} is a shell builtin");
+            else
+                Console.WriteLine($"{cmdArgs[1]}: not found");
+            break;
+        default:
+            Console.WriteLine($"{command}: command not found");
+            break;
     }
 }
