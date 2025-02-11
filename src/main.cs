fa84a713e47446c29eb1b0e83a7a8b409df9b8e1
keeperofpictures <abuzersfzxc@gmail.com>
2025-02-11 19:09:40 +0300

    step 4th

--- src/main.cs
+++ src/main.cs
@@ -5,7 +5,18 @@ while (true)
 {
     Console.Write("$ ");
     var command = Console.ReadLine().Trim();
-    if (command == "exit 0")
-        return 0;
-    Console.WriteLine($"{command}: command not found");
+    if (command.StartsWith("exit"))
+    {
+        var cmdArgs = command.Split();
+        return int.Parse(cmdArgs[1]);
+    }
+    else if (command.StartsWith("echo"))
+    {
+        if(command.Length > 5)
+            Console.WriteLine(command.Substring(5));
+    }
+    else
+    {
+        Console.WriteLine($"{command}: command not found");
+    }
 }
